0a0bfa42e22e7f10bed94d7bcc6b5fef680631e4
SAHA-YJB <pkomom123@naver.com>
2023-07-08 15:18:26 +0900

    버튼 완료

--- src/components/Buttons/ButtonsElements.js
+++ src/components/Buttons/ButtonsElements.js
@@ -1,18 +1,39 @@
 import { styled } from "styled-components";
 
 export const Buttonspage = styled.div`
-background-color: #1f2123;
-height: 860px;
-width:100%;
-margin-right: auto;
-margin-left: auto;
-padding: 0 24px;
-justify-content: center;
-
-`
+  background-color: #1f2123;
+  height: 860px;
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  padding: 0 24px;
+  justify-content: center;
+`;
 
 export const ButtonspageName = styled.h1`
-text-align: center;
-background-color: aliceblue;
+  text-align: center;
+  background-color: aliceblue;
+`;
 
-`
+export const BtnLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 100px;
+  margin-top: 200px;
+`;
+
+export const ButtonComponent = styled.button`
+  background-color: ${(props) => props.backgroundColor};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+  color: ${(props) => props.color};
+  border-radius: ${(props) => props.borderRadius};
+  border: ${(props) => props.border};
+  cursor: pointer;
+
+  &:active {
+    background-color: #fff;
+  }
+`;
--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,12 +1,105 @@
-import React from 'react'
-import { ButtonspageName, Buttonspage } from './ButtonsElements';
+import React from "react";
+import AppleIcon from "@mui/icons-material/Apple";
+import {
+  ButtonspageName,
+  Buttonspage,
+  ButtonComponent,
+  BtnLayout,
+} from "./ButtonsElements";
 
 const Buttons = () => {
+  const onBtnAlert = () => {
+    alert("버튼 만들기");
+  };
+  const onBtnPrompt = () => {
+    if (window.prompt("애플 제품 씁니당 뭐 쓰시나용?")) {
+      alert("멋지십니다!");
+    }
+  };
   return (
-    <Buttonspage id='buttons'>
-    <ButtonspageName>Buttonspage</ButtonspageName>
+    <Buttonspage id="buttons">
+      <ButtonspageName>Buttonspage</ButtonspageName>
+      <BtnLayout>
+        <ButtonComponent
+          backgroundColor="black"
+          width="280px"
+          height="50px"
+          fontSize="22px"
+          fontWeight="900"
+          color="#fc007a"
+          borderRadius="10px"
+          border="1px solid #fc007a"
+          onClick={onBtnAlert}
+        >
+          Large Prime Button
+        </ButtonComponent>
+        <ButtonComponent
+          backgroundColor="#29140F"
+          width="180px"
+          height="50px"
+          fontSize="20px"
+          fontWeight="700"
+          color="#fc007a"
+          borderRadius="10px"
+          border="1px solid #fc007a"
+        >
+          MediumBtn
+        </ButtonComponent>
+        <ButtonComponent
+          backgroundColor="#3C2F2B"
+          width="100px"
+          height="50px"
+          fontSize="16px"
+          fontWeight="400"
+          color="#fc007a"
+          borderRadius="10px"
+          border="1px solid #fc007a"
+        >
+          SmallBtn
+        </ButtonComponent>
+      </BtnLayout>
+      <BtnLayout>
+        <ButtonComponent
+          backgroundColor="#F6BB43"
+          width="280px"
+          height="50px"
+          fontSize="22px"
+          fontWeight="900"
+          color="#427EF6"
+          borderRadius="10px"
+          border="1px solid #42E9F6"
+          onClick={onBtnPrompt}
+        >
+          Large Icon Button
+          <AppleIcon />
+        </ButtonComponent>
+        <ButtonComponent
+          backgroundColor="	#F9D68E"
+          width="180px"
+          height="50px"
+          fontSize="20px"
+          fontWeight="700"
+          color="#427EF6"
+          borderRadius="10px"
+          border="1px solid #42E9F6"
+        >
+          MediumBtn
+        </ButtonComponent>
+        <ButtonComponent
+          backgroundColor="	#F9D68E"
+          width="100px"
+          height="50px"
+          fontSize="16px"
+          fontWeight="400"
+          color="#427EF6"
+          borderRadius="10px"
+          border="1px solid #42E9F6"
+        >
+          SmallBtn
+        </ButtonComponent>
+      </BtnLayout>
     </Buttonspage>
-  )
-}
+  );
+};
 
 export default Buttons;
